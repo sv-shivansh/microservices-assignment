ae5b7f896315a5a837b0ab84a35d497032b6fe76
sv-shivansh <sv_shivansh@outlook.com>
2022-01-28 01:19:13 +0530

    [WIP] restructuring for microservice architecture

---
+++ microservice_assignment/User/User.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./db");
+require("dotenv").config();
+
+const PORT = process.env.USER_PORT || 8080;
+// connecting databse
+connectDB();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.status(200).send("User sample service response");
+});
+
+app.use("/api/v1/users", require("./routes/user"));
+
+app.listen(PORT, () => {
+    console.log(`User Service Server is running at ${PORT}`);
+});
--- microservice_assignment/utility/auth.js
+++ microservice_assignment/utility/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("./models/user-model");
 module.exports = async function (req, res, next) {
     const user = req.header("user");
     if (!user) {
